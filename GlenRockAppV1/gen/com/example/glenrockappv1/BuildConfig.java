a165041b25fb43e9ab1cd60d0a24a05d67d69ae3
ichugwindex <noelw55@gmail.com>
2013-10-13 23:26:42 -0400

    Added Actionbarsherlock, Menu, (Partial News), and mockups

--- GlenRockAppV1/gen/com/example/glenrockappv1/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.glenrockappv1;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
---
+++ GlenRockAppV1/src/com/glenrockappv1/NewsFragment.java
@@ -0,0 +1,55 @@
+package com.glenrockappv1;
+
+import java.util.ArrayList;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.actionbarsherlock.app.SherlockListFragment;
+
+public class NewsFragment extends SherlockListFragment {
+	//future handle fragment navigation
+	//	public interface EntitySelectedListener {
+	//		public void onEntitySelected(String id, boolean type);
+	//	}
+	//EntitySelectedListener mListener;
+	public ArrayList<String> stockNewsTitles;
+	public ArrayList<String> stockNewsSnipps;
+	public ArrayList<Integer> nid;
+	public ArrayList<String> titles;
+	public NewsListAdapter adapter;
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_news,
+				container, false);
+		return view;
+	}
+
+	@Override
+	public void onStart(){
+		super.onStart();
+		//Very hackish way to exchange this data, will find better solution later
+		stockNewsTitles = ((MainActivity)getActivity()).stockNewsTitles;
+		stockNewsSnipps = ((MainActivity)getActivity()).stockNewsSnipps;
+		//initialize all nids to 0 since theyre unused right now
+		for (int i = 0; i < 6; i++){
+			//nid.set(0,  0);
+			Log.i("titles", stockNewsTitles.get(i));
+		}
+		adapter = new NewsListAdapter(getActivity(), stockNewsTitles, stockNewsSnipps, nid);
+		//fill listview
+		setListAdapter(adapter);
+	}
+
+
+	@Override
+	public void onStop() {
+		super.onStop();
+	}
+
+}
+
