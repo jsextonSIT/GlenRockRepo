2ddedd109e0275c96e9c325bb92b13c3f9fff51a
Suketu Shah <stevenssshah75@gmail.com>
2013-11-10 12:34:00 -0500

    Delete this after I commit

--- Hello World/MyApplication/src/main/gen/com/myapplication/BuildConfig.java
+++ Hello World/MyApplication/src/main/gen/com/myapplication/BuildConfig.java
@@ -1,7 +1,7 @@
 /*___Generated_by_IDEA___*/
 
 /** Automatically generated file. DO NOT MODIFY */
-package com.glenrockappv1;
+package com.myapplication;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
